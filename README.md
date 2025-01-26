b947d820f66fa0a9a0e29a014e58fd996d574797
Brums21 <75539238+Brums21@users.noreply.github.com>
2025-01-26 22:57:02 +0000

    added script to get all tools

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # benchmark
+
+## Script to download tools
+
+The file `get_tools.sh` allows to download all tools, except for GeneMark-ES and GeneMark-EP+, which should be downloaded from [here](http://topaz.gatech.edu/GeneMark/license_download.cgi). The option considered for this benchmarking test is `GeneMark-ES/ET/EP+ ver 4.72_lic` for the LINUX 64 OS. Be careful to download both the software and the license key, placing the last one in the same directory as the software folder.
+
+### Steps to setup the GeneMark-ES and GeneMark-EP+ tools:
+
+1. Download the software and license key using the `wget [copied software link]` command. 
+
+2. Unzip the software using the `tar -xvzf gmes_linux_64.tar.gz`
+
+3. Remove the compressed folder: `rm gmes_linux_64.tar.gz`
+
+4. Get the license key, copying the link from the download: `wget [copied license link]`
+
+5. Unzip the license key: `gunzip gm_key_64.gz`
+
+6. Place the license key in the root folder of the software: `mv gm_key_64 gmes_linux_64/`
---
+++ get_tools.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Check if wget and unzip are installed
+command -v wget >/dev/null 2>&1 || { 
+    echo "wget is required but not installed. Installing..."; 
+    apt-get install unzip;
+}
+command -v unzip >/dev/null 2>&1 || { 
+    echo "Unzip is required but not installed. Installing...";
+    apt-get install unzip;
+}
+
+# GeneMark-ETP
+echo -e "\n\nGeneMark-ETP ------------------------------------------------------"
+if [ ! -d "GeneMark-ETP" ]; then
+    echo "Installing GeneMark-ETP..."
+    wget https://github.com/gatech-genemark/GeneMark-ETP/releases/download/etp-v1.02-preprint/GeneMark-ETP.tar.gz -O GeneMark-ETP.tar.gz
+    tar -xvzf GeneMark-ETP.tar.gz
+    rm GeneMark-ETP.tar.gz
+else
+    echo "GeneMark-ETP is already installed. Skipping..."
+fi
+
+# GeneMark-ES and GeneMark-EP+ -> see README.md
+
+# AUGUSTUS
+echo -e "\n\nAUGUSTUS ------------------------------------------------------"
+if [ ! -d "Augustus-3.5.0" ]; then
+    echo "Instaling AUGUSTUS..."
+    wget https://github.com/Gaius-Augustus/Augustus/archive/refs/tags/v3.5.0.tar.gz -O Augustus-3.5.0.tar.gz
+    tar -xvzf Augustus-3.5.0.tar.gz
+    rm Augustus-3.5.0.tar.gz
+else
+    echo "AUGUSTUS is already installed. Skipping..."
+fi
+
+# Seqping
+echo -e "\n\nSeqping ------------------------------------------------------"
+if [ ! -d "seqping_0.1.45.1" ]; then
+    echo "Installing Seqping..."
+    wget https://sourceforge.net/projects/seqping/files/latest/download -O Seqping_0.1.45.1.tar.gz
+    tar -xvzf Seqping_0.1.45.1.tar.gz
+    rm Seqping_0.1.45.1.tar.gz
+else
+    echo "Seqping is already installed. Skipping..."
+fi
+
+# SNAP
+echo -e "\n\nSNAP ------------------------------------------------------"
+if [ ! -d "SNAP-master" ]; then
+    echo "Instaling SNAP..."
+    wget https://github.com/KorfLab/SNAP/archive/refs/heads/master.zip -O SNAP.zip
+    unzip SNAP.zip
+else
+    echo "SNAP is already installed. Skipping..."
+fi
+
+# GeMoMa
+echo -e "\n\nGeMoMa ------------------------------------------------------"
+if [ ! -d "GeMoMa" ]; then
+    echo "Installing GeMoMa..."
+    wget http://www.jstacs.de/download.php?which=GeMoMa -O GeMoMa.zip
+    mkdir GeMoMa
+    mv GeMoMa.zip GeMoMa/
+    cd GeMoMa
+    unzip GeMoMa.zip
+    rm GeMoMa.zip
+    cd ..
+else 
+    echo "GeMoMa is already installed. Skipping..."
+fi 
+
+echo -e "\n\nFinished installing all tools"
