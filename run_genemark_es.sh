8b8076959aeba1a980c1e894563e23c6107d1990
Brums21 <75539238+Brums21@users.noreply.github.com>
2025-01-29 17:24:41 +0000

    error in results

--- run_genemark_es.sh
+++ run_genemark_es.sh
@@ -8,17 +8,20 @@ for SPECIES in "$SPECIES_FOLDER"/*; do
     if [ -d "$SPECIES" ]; then
         echo "Species being processed: $SPECIES"
 
+        mkdir -p $SPECIES; cd $SPECIES;
+
         DNA_FILE="$SPECIES/$(basename "$SPECIES")_dna.fa"
 
         start_time=$(date +%s)
 
-        ../../gmes_linux_64/gmes_petap.pl --sequence $DNA_FILE --ES
+        ../../../gmes_linux_64/gmes_petap.pl --sequence $DNA_FILE --ES --cores 8
 
         end_time=$(date +%s)
         elapsed_time=$((end_time - start_time))
 
-        echo "Process took $elapsed_time seconds." > "${SPECIES_NAME}_time.txt"
-        echo "Elapsed time for $SPECIES_NAME: $elapsed_time seconds."
+        echo "Process took $elapsed_time seconds." > "../../../${SPECIES}_time.txt"
+        echo "Elapsed time for $SPECIES: $elapsed_time seconds."
 
+        cd ..
     fi
 done
